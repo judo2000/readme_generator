0d0acf9f88f70c67b5df2e21f5cdd489cc8c9b9e
Stephen Moore <judo2000@comcast.net>
2021-12-20 17:04:32 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const readmeBuilder = require('./readmeBuilder.js');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'project_title',
+    },
+    {
+      type: 'input',
+      message: 'Please provide a description of your project:',
+      name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please provide installation instructions:',
+        name: 'installInstructions',
+    },
+    {
+        type: 'input',
+        message: 'Please provide usage information if applicable:',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your Github username:',
+        name: 'githubUsername',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email address:',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email address:',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    },
+  ])
+  .then((response) => {
+    
+    fs.writeFile('readme.md', readmeBuilder(response), (err) =>
+        err ? console.error(err) : console.log('Success!')
+    )
+  });
---
+++ readme.md
@@ -0,0 +1,40 @@
+<div id="top"></div>
+
+    <h3 align="center">undefined</h3>
+  </div>
+
+  ![GitHub license](Mozilla Public License 2.0)
+  
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation Instructions](#installation-instructions)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Questions](#questions)
+  
+  
+  ## Description
+  test
+
+  <p align="right">(<a href="#top">back to top</a>)</p>
+
+  ## Installation Instructions
+  test
+  
+  <p align="right">(<a href="#top">back to top</a>)</p>
+  
+  ## Usage
+
+  test
+
+  <p align="right">(<a href="#top">back to top</a>)</p>
+
+  ## License
+  [![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+    
+  ## Questions
+
+  [https://github.com/test](https://github.com/test)
+  [test](test)
+
+  <p align="right">(<a href="#top">back to top</a>)</p>
